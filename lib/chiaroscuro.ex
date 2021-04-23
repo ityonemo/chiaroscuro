1b87a9dfc3fdb9c2abfa5d7d6087f3e607650448
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2021-04-22 23:17:46 -0700

    adds elixir bootstrap

---
+++ lib/chiaroscuro.ex
@@ -0,0 +1,18 @@
+defmodule Chiaroscuro do
+  @moduledoc """
+  Documentation for `Chiaroscuro`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Chiaroscuro.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/chiaroscuro_test.exs
@@ -0,0 +1,8 @@
+defmodule ChiaroscuroTest do
+  use ExUnit.Case
+  doctest Chiaroscuro
+
+  test "greets the world" do
+    assert Chiaroscuro.hello() == :world
+  end
+end
